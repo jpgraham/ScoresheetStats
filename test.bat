cfa2f21f2f82489db36e1f5f94b7eddb5d454e21
jpgraham <37601792+jpgraham@users.noreply.github.com>
2018-04-02 10:02:05 -0700

    Delete test.bat

--- test.bat
+++
@@ -1,6 +0,0 @@
-echo off
-title My Test Batch File
-:: See the title at the top. And this comment will not appear in the command prompt.
-echo Test file executed.
-echo I am too lazy to write commands again and again.
-pause
