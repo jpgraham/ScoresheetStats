4c723a4f871de45885d3396e3c99dca1530d0c31
jpgraham <37601792+jpgraham@users.noreply.github.com>
2018-04-03 09:54:23 -0700

    Update send_email.R

--- code/send_email.R
+++ code/send_email.R
@@ -2,9 +2,16 @@
 ###Date: March 30, 2018 
 ###This file creates an HTML report of scoresheet stats and emails the results
 
+#load packages
+requirements<- c("dplyr", "knitr", "data.table", "rvest", "XML", "magrittr", "mailR")
+
+for(requirement in requirements){if(!(requirement %in% installed.packages()))
+  install.packages(requirement)}
+lapply(requirements, require, character.only=T)
+
 #Create Report
 source('scoresheet_logs.R')
-library(mailR)
+
 #send email
 date<-format(Sys.Date(), format="%B %d %Y")
 sender <- "sender@gmail.com" # Replace with a valid address
