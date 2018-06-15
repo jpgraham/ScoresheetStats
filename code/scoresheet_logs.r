e3e01224203cc1b44b13b025e243b0ca6b1e3df8
jpgraham <37601792+jpgraham@users.noreply.github.com>
2018-06-15 13:27:50 -0700

    Update scoresheet_logs.r

--- code/scoresheet_logs.r
+++ code/scoresheet_logs.r
@@ -7,11 +7,11 @@ lapply(requirements, require, character.only=T)
 
 
 #--------------------Create game log for position players-----------------------------
+
 #Create table of players and IDs for fangraph links
 player_id<-read.csv("../input/SS_position_players.csv", header=TRUE)
 unique_id<-unique(player_id[,1:2]) #remove duplicate players (both teams)
-my_data=NULL
-
+position=NULL
 #Scrape data and combine into a single file
 for (i in unique_id[,2]) {
 test=try(read_html(paste("http://www.fangraphs.com/statsd.aspx?playerid=",i,"&season=2018",sep="",collapse=NULL)),TRUE)
@@ -22,36 +22,28 @@ x<-x %>%
 if(length(x) != 0) { 
 x<-subset(data.frame(x), !(data.frame(x)$Date=="Date" | data.frame(x)$Date=="Total"))#converts to a data frame and drops duplicate headers and season totals
 x$BIS<-as.numeric(unique_id[match(i,unique_id$BIS),2]) #Add BIS Number from player list
-x$BO<-as.numeric(x$BO)
-x$PA<-as.numeric(x$PA)
-x$H<-as.numeric(x$H)
-x$X2B<-as.numeric(x$X2B)
-x$X3B<-as.numeric(x$X3B)
-x$HR<-as.numeric(x$HR)
-x$R<-as.numeric(x$R)
-x$RBI<-as.numeric(x$RBI)
-x$SB<-as.numeric(x$SB)
-x$CS<-as.numeric(x$CS)
+
+#Clean up data. Create walks and strike out fields
+
+cols<-c("BO", "PA", "H","X2B","X3B","HR","R","RBI","SB","CS", "ISO", "BABIP")
+x[,cols] <- as.numeric(as.character(unlist(x[,cols])))
+
 x$BB.<-as.numeric(gsub("%","", x$BB.))
 x$K.<-as.numeric(gsub("%","", x$K.))
-x$ISO<-as.numeric(x$ISO)
-x$BABIP<-as.numeric(x$BABIP)
 x$BB<-round(x$PA*x$BB./100)
-x$K<-round(x$PA*x$K./100)
-my_data<-rbind(x,my_data)
+x$SO<-round(x$PA*x$K./100)
+position<-rbind(x,position)
                       }
 }
 }
-write.csv(my_data, "../output/position_logs.csv", row.names=FALSE) #Export data to csv file
-
 
 
 #--------------------Create game log for pitchers-----------------------------
+
 #Create table of players and IDs for fangraph links
 pitcher_id<-read.csv("../input/SS_pitchers.csv", header=TRUE)
 unique_pitcher<-unique(pitcher_id[,1:2]) #remove duplicate players (both teams)
-my_data=NULL
-
+pitcher=NULL
 #Scrape data and combine into a single file
 for (i in unique_pitcher[,2]) {
 test=try(read_html(paste("http://www.fangraphs.com/statsd.aspx?playerid=",i,"&season=2018",sep="",collapse=NULL)),TRUE)
@@ -62,21 +54,19 @@ x<-x %>%
 if(length(x) != 0) { 
 x<-subset(data.frame(x), !(data.frame(x)$Date=="Date" | data.frame(x)$Date=="Total"))#converts to a data frame and drops duplicate headers and season totals
 x$BIS<-as.numeric(unique_pitcher[match(i,unique_pitcher$BIS),2]) #Add BIS Number order from player list
-x$IP<-as.numeric(x$IP)
-x$H<-as.numeric(x$H)
-x$ER<-as.numeric(x$ER)
-x$HR<-as.numeric(x$HR)
-x$BB<-as.numeric(x$BB)
-x$SO<-as.numeric(x$SO)
-x$BABIP<-as.numeric(x$BABIP)
-my_data<-rbind(x,my_data)
+
+#Clean up data
+
+cols<-c("IP", "H","ER", "HR","BB","SO", "BABIP")
+x[,cols] <- as.numeric(as.character(unlist(x[,cols])))
+pitcher<-rbind(x,pitcher)
                       }
 }
 }
-write.csv(my_data, "../output/pitcher_logs.csv", row.names=FALSE)
 
-#Scrape minor league data
-source('minor_game_logs.r')
+
+#Pull Minor League Data
+source('C:/Users/Jeff/Documents/Scoresheet Stats - Private/code/minor_game_logs.r')
 
 Sys.setenv(RSTUDIO_PANDOC="C:/Program Files/RStudio/bin/pandoc")
 rmarkdown::render("ss_analysis.Rmd", "html_document")
